4a51a5882a4a975e99b4590469357eac4d1d21a2
Your Name <jymohcooler@gmail.com>
2023-06-11 14:38:49 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const down = [10,9,8,7,6,5,4,3,2,1];
+function countDown(down) {
+let countdown = 10;
+while (countdown >= 0) {
+console.log(countdown--);
+}
+return down;
+}
+countDown(down);
